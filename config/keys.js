64693166aee15c31eee28a0720b3befa3c362881
Awonusi <awonusiolajide@yahoo.com>
2018-09-13 13:04:10 +0100

    added concurrently

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
     mongoURI:
-    'mongodb://amjide:abraham2@ds151282.mlab.com:51282/devconector'
+    'mongodb://amjide:abraham2@ds151282.mlab.com:51282/devconector',
+    secretOrKey: 'secret'
 }
