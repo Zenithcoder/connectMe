78a06efa61304a60c8a605e0a50737db69ed8e36
Awonusi <awonusiolajide@yahoo.com>
2018-09-17 11:20:33 +0100

    moment installation

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-    mongoURI:
-    'mongodb://amjide:abraham2@ds151282.mlab.com:51282/devconector',
-    secretOrKey: 'secret'
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
 }
---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: 'mongodb://amjide:abraham2@ds151282.mlab.com:51282/devconector',
+  secretOrKey: 'secret'
+};
